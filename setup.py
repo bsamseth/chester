c6dde1a2a3e48f57ccea5db7519ffadbc8e3b38c
Bendik Samseth <b.samseth@gmail.com>
2019-02-07 18:28:20 +0100

    Update version number to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="chester",
-    version="0.1.0",
+    version="0.2.0",
     author="Bendik Samseth",
     author_email="b.samseth@gmail.com",
     description="Chess Engine Tester - A simple interface to play chess engines against each other, including tournaments.",
