30452ac61efd2955b27be1ef7fb0186d33f48a70
Bendik Samseth <b.samseth@gmail.com>
2019-02-19 16:53:17 +0100

    Bump version number and update install reqs.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="chester",
-    version="0.3.0",
+    version="0.3.1",
     author="Bendik Samseth",
     author_email="b.samseth@gmail.com",
     description="Chess Engine Tester - A simple interface to play chess engines against each other, including tournaments.",
@@ -13,7 +13,7 @@ setup(
     packages=["chester"],
     long_description=open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
     long_description_content_type="text/markdown",
-    install_requires=["python-chess>=0.25.1"],
+    install_requires=["python-chess>=0.26.0"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
